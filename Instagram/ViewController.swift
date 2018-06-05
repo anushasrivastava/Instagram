feca0d5093cf3f23e1ca1a590d44dd8a9b3f7e4f
Anusha Srivastava <anushasrivastava@gmail.com>
2018-06-05 11:33:37 -0500

    Initial Commit

---
+++ Instagram/ViewController.swift
@@ -0,0 +1,38 @@
+//
+//  ViewController.swift
+//  Instagram
+//
+//  Created by Anusha Srivastava on 6/5/18.
+//  Copyright © 2018 Anusha Srivastava. All rights reserved.
+//
+
+import UIKit
+import Parse
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        let gameScore = PFObject(className:"GameScore")
+        gameScore["score"] = 1337
+        gameScore["playerName"] = "Sean Plott"
+        gameScore["cheatMode"] = false
+        gameScore.saveInBackground {
+            (success: Bool, error: Error?) in
+            if (success) {
+                print("Success")
+            } else {
+               print("Failed")
+            }
+        }
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
